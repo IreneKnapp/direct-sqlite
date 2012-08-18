bbab909dd3399759adc2e0ad363e4bf1373decaf
Joey Adams <joeyadams3.14159@gmail.com>
2012-08-18 05:19:36 -0400

    Database.SQLite3.Direct: Add Utf8 type, and implement 'open'

--- Database/SQLite3/Direct.hs
+++ Database/SQLite3/Direct.hs
@@ -13,7 +13,8 @@ module Database.SQLite3.Direct (
     StepResult(..),
     Error(..),
 
-    -- ** Special integer types
+    -- ** Special types
+    Utf8(..),
     ParamIndex(..),
     ColumnIndex(..),
     ColumnCount(..),
@@ -55,13 +56,14 @@ module Database.SQLite3.Direct (
 
 import Database.SQLite3.Bindings
 
-import Prelude hiding (error)
-import qualified Prelude
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Internal as BSI
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import Control.Applicative ((<$>))
+import Control.Applicative  ((<$>))
+import Control.Monad
+import Data.ByteString      (ByteString)
+import Data.String          (IsString(..))
 import Data.Typeable
 import Foreign
 import Foreign.C
@@ -86,19 +88,36 @@ data Result a
 data SQLData
     = SQLInteger    !Int64
     | SQLFloat      !Double
-    | SQLText       !T.Text
-    | SQLBlob       !BS.ByteString
+    | SQLText       !Utf8
+    | SQLBlob       !ByteString
     | SQLNull
     deriving (Eq, Show, Typeable)
 
--- | Convert a 'CError' to a 'Result', in the common case where @SQLITE_OK@
--- signals success and anything else signals an error.
-toResult :: CError -> a -> Result a
-toResult code@(CError n) a =
+-- | A 'ByteString' containing UTF8-encoded text.
+newtype Utf8 = Utf8 ByteString
+    deriving (Eq, Show)
+
+instance IsString Utf8 where
+    fromString = Utf8 . T.encodeUtf8 . T.pack
+
+errorResult :: CError -> Result a
+errorResult code@(CError n) =
     case decodeError code of
-        Just ErrorOK -> Okay a
-        Just err     -> Error err
-        Nothing      -> UnknownError (fromIntegral n)
+        Just err -> Error err
+        Nothing  -> UnknownError (fromIntegral n)
+
+-- Convert a 'CError' to a 'Result', in the common case where
+-- SQLITE_OK signals success and anything else signals an error.
+--
+-- Note that SQLITE_OK == 0.
+toResult :: CError -> a -> Result a
+toResult (CError 0) a = Okay a
+toResult code       _ = errorResult code
+
+-- Only perform the action if the 'CError' is SQLITE_OK.
+toResultM :: Monad m => CError -> m a -> m (Result a)
+toResultM (CError 0) m = m >>= return . Okay
+toResultM code       _ = return (errorResult code)
 
 toStepResult :: CError -> Result StepResult
 toStepResult code@(CError n) =
@@ -116,8 +135,12 @@ toColumnType code@(CColumnType n) =
 
 ------------------------------------------------------------------------
 
-open :: String -> IO Database
-open = undefined
+open :: Utf8 -> IO (Result Database)
+open (Utf8 path) =
+    BS.useAsCString path $ \path' ->
+        alloca $ \database -> do
+            rc <- c_sqlite3_open path' database
+            toResultM rc (Database <$> peek database)
 
 close :: Database -> IO ()
 close = undefined
@@ -161,10 +184,10 @@ bindInt64 = undefined
 bindDouble :: Statement -> Int -> Double -> IO ()
 bindDouble = undefined
 
-bindText :: Statement -> Int -> T.Text -> IO ()
+bindText :: Statement -> Int -> Utf8 -> IO ()
 bindText = undefined
 
-bindBlob :: Statement -> Int -> BS.ByteString -> IO ()
+bindBlob :: Statement -> Int -> ByteString -> IO ()
 bindBlob = undefined
 
 bindNull :: Statement -> Int -> IO ()
@@ -186,8 +209,8 @@ columnInt64 = undefined
 columnDouble :: Statement -> Int -> IO Double
 columnDouble = undefined
 
-columnText :: Statement -> Int -> IO T.Text
+columnText :: Statement -> Int -> IO Utf8
 columnText = undefined
 
-columnBlob :: Statement -> Int -> IO BS.ByteString
+columnBlob :: Statement -> Int -> IO ByteString
 columnBlob = undefined
