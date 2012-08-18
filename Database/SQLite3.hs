68788793f310ea9f70b8e8996589e9a89fd75524
Joey Adams <joeyadams3.14159@gmail.com>
2012-08-18 09:55:48 -0400

    Update Database.SQLite3 to use Database.SQLite3.Direct, (hopefully) without breaking the API

--- Database/SQLite3.hs
+++ Database/SQLite3.hs
@@ -33,24 +33,32 @@ module Database.SQLite3 (
                         )
     where
 
-import Database.SQLite3.Bindings
+import Database.SQLite3.Direct
+    ( Database
+    , Statement
+    , Error(..)
+    , StepResult(..)
+    , ColumnType(..)
+    , Utf8(..)
+    )
+import Database.SQLite3.Bindings.Types
+    ( fromParamIndex, toParamIndex
+    , toColumnIndex
+    , fromColumnCount
+    )
+
+import qualified Database.SQLite3.Direct as Direct
 
 import Prelude hiding (error)
 import qualified Prelude
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Internal as BSI
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import Control.Applicative ((<$>))
+import Control.Applicative  ((<$>))
+import Data.Int             (Int64)
+import Data.String          (fromString)
 import Data.Typeable
-import Foreign
-import Foreign.C
 
-
-newtype Database  = Database  (Ptr CDatabase)
-newtype Statement = Statement (Ptr CStatement)
-
-data StepResult = Row | Done deriving (Eq, Show)
 
 data SQLData = SQLInteger Int64
              | SQLFloat Double
@@ -59,12 +67,14 @@ data SQLData = SQLInteger Int64
              | SQLNull
                deriving (Eq, Show, Typeable)
 
+fromUtf8 :: Utf8 -> String
+fromUtf8 (Utf8 bs) = (T.unpack . T.decodeUtf8) bs
+
+toUtf8 :: String -> Utf8
+toUtf8 = fromString
 
 errmsg :: Database -> IO String
-errmsg (Database database) = do
-  message <- c_sqlite3_errmsg database
-  byteString <- BS.packCString message
-  return $ T.unpack $ T.decodeUtf8 byteString
+errmsg db = fromUtf8 <$> Direct.errmsg db
 
 sqlError :: Maybe Database -> String -> Error -> IO a
 sqlError maybeDatabase functionName error = do
@@ -77,104 +87,42 @@ sqlError maybeDatabase functionName error = do
          ++ " while attempting to perform " ++ functionName
          ++ details
 
-openError :: String -> IO (Either Database Error)
-openError path = do
-  BS.useAsCString (T.encodeUtf8 $ T.pack path)
-                  (\path -> do
-                     alloca (\database -> do
-                               error <- c_sqlite3_open path database
-                               error <- return $ decodeError error
-                               case error of
-                                 ErrorOK -> do
-                                            database <- peek database
-                                            return $ Left $ Database database
-                                 _ -> return $ Right error))
+checkError :: Maybe Database -> String -> Either Error a -> IO a
+checkError db fn = either (sqlError db fn) return
+
 open :: String -> IO Database
 open path = do
-  databaseOrError <- openError path
-  case databaseOrError of
-    Left database -> return database
-    Right error -> sqlError Nothing ("open " ++ show path) error
+    Direct.open (toUtf8 path)
+        >>= checkError Nothing ("open " ++ show path)
 
-closeError :: Database -> IO Error
-closeError (Database database) = do
-  error <- c_sqlite3_close database
-  return $ decodeError error
 close :: Database -> IO ()
-close database = do
-  error <- closeError database
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError (Just database) "close" error
+close db =
+    Direct.close db >>= checkError (Just db) "close"
 
-prepareError :: Database -> String -> IO (Either Statement Error)
-prepareError (Database database) text = do
-  BS.useAsCString (T.encodeUtf8 $ T.pack text)
-                  (\text -> do
-                     alloca (\statement -> do
-                               error <- c_sqlite3_prepare_v2 database
-                                                             text
-                                                             (CNumBytes (-1))
-                                                             statement
-                                                             nullPtr
-                               error <- return $ decodeError error
-                               case error of
-                                 ErrorOK -> do
-                                            statement <- peek statement
-                                            return $ Left $ Statement statement
-                                 _ -> return $ Right error))
 prepare :: Database -> String -> IO Statement
-prepare database text = do
-  statementOrError <- prepareError database text
-  case statementOrError of
-    Left statement -> return statement
-    Right error -> sqlError (Just database) ("prepare " ++ (show text)) error
+prepare db sql =
+    Direct.prepare db (toUtf8 sql) >>=
+        checkError (Just db) ("prepare " ++ (show sql))
 
-stepError :: Statement -> IO Error
-stepError (Statement statement) = do
-  error <- c_sqlite3_step statement
-  return $ decodeError error
 step :: Statement -> IO StepResult
-step statement = do
-  error <- stepError statement
-  case error of
-    ErrorRow -> return Row
-    ErrorDone -> return Done
-    _ -> sqlError Nothing "step" error
+step statement =
+    Direct.step statement >>= checkError Nothing "step"
 
-resetError :: Statement -> IO Error
-resetError (Statement statement) = do
-  error <- c_sqlite3_reset statement
-  return $ decodeError error
 reset :: Statement -> IO ()
-reset statement = do
-  error <- resetError statement
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "reset" error
+reset statement =
+    Direct.reset statement >>= checkError Nothing "reset"
 
-finalizeError :: Statement -> IO Error
-finalizeError (Statement statement) = do
-  error <- c_sqlite3_finalize statement
-  return $ decodeError error
 finalize :: Statement -> IO ()
-finalize statement = do
-  error <- finalizeError statement
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "finalize" error
+finalize statement =
+    Direct.finalize statement >>= checkError Nothing "finalize"
 
 
 -- | This returns the index of the largest (rightmost) parameter, which is not
 -- necessarily the number of parameters.  If numbered parameters like @?5@ are
 -- used, there may be gaps in the list.
 bindParameterCount :: Statement -> IO Int
-bindParameterCount (Statement stmt) =
-  fromParamIndex <$> c_sqlite3_bind_parameter_count stmt
-
-maybeNullCString :: CString -> IO (Maybe BS.ByteString)
-maybeNullCString s =
-  if s == nullPtr then return Nothing else Just <$> BS.packCString s
+bindParameterCount stmt =
+    fromParamIndex <$> Direct.bindParameterCount stmt
 
 -- | Return the N-th SQL parameter name.
 --
@@ -184,97 +132,45 @@ maybeNullCString s =
 --
 -- Note that the parameter index starts at 1, not 0.
 bindParameterName :: Statement -> Int -> IO (Maybe String)
-bindParameterName (Statement stmt) idx = do
-  mn <- c_sqlite3_bind_parameter_name stmt (toParamIndex idx) >>= maybeNullCString
-  return (mn >>= return . T.unpack . T.decodeUtf8)
+bindParameterName stmt idx =
+    fmap fromUtf8 <$>
+    Direct.bindParameterName stmt (toParamIndex idx)
 
-bindBlobError :: Statement -> Int -> BS.ByteString -> IO Error
-bindBlobError (Statement statement) parameterIndex byteString = do
-  size <- return $ BS.length byteString
-  BS.useAsCString byteString
-                  (\dataC -> do
-                     error <- c_sqlite3_bind_blob statement
-                                                  (toParamIndex parameterIndex)
-                                                  dataC
-                                                  (toCNumBytes size)
-                                                  c_SQLITE_TRANSIENT
-                     return $ decodeError error)
 bindBlob :: Statement -> Int -> BS.ByteString -> IO ()
-bindBlob statement parameterIndex byteString = do
-  error <- bindBlobError statement parameterIndex byteString
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind blob" error
+bindBlob statement parameterIndex byteString =
+    Direct.bindBlob statement (toParamIndex parameterIndex) byteString
+        >>= checkError Nothing "bind blob"
 
-bindDoubleError :: Statement -> Int -> Double -> IO Error
-bindDoubleError (Statement statement) parameterIndex datum = do
-  error <- c_sqlite3_bind_double statement
-                                 (toParamIndex parameterIndex)
-                                 datum
-  return $ decodeError error
 bindDouble :: Statement -> Int -> Double -> IO ()
-bindDouble statement parameterIndex datum = do
-  error <- bindDoubleError statement parameterIndex datum
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind double" error
-
-bindIntError :: Statement -> Int -> Int -> IO Error
-bindIntError (Statement statement) parameterIndex datum = do
-  error <- c_sqlite3_bind_int64 statement
-                                (toParamIndex parameterIndex)
-                                (fromIntegral datum)
-  return $ decodeError error
+bindDouble statement parameterIndex datum =
+    Direct.bindDouble statement (toParamIndex parameterIndex) datum
+        >>= checkError Nothing "bind double"
 
 bindInt :: Statement -> Int -> Int -> IO ()
-bindInt statement parameterIndex datum = do
-  error <- bindIntError statement parameterIndex datum
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind int" error
+bindInt statement parameterIndex datum =
+    Direct.bindInt64 statement
+                     (toParamIndex parameterIndex)
+                     (fromIntegral datum)
+        >>= checkError Nothing "bind int"
 
-bindInt64Error :: Statement -> Int -> Int64 -> IO Error
-bindInt64Error (Statement statement) parameterIndex datum = do
-  error <- c_sqlite3_bind_int64 statement
-                                (toParamIndex parameterIndex)
-                                datum
-  return $ decodeError error
 bindInt64 :: Statement -> Int -> Int64 -> IO ()
-bindInt64 statement parameterIndex datum = do
-  error <- bindInt64Error statement parameterIndex datum
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind int64" error
+bindInt64 statement parameterIndex datum =
+    Direct.bindInt64 statement
+                     (toParamIndex parameterIndex)
+                     datum
+        >>= checkError Nothing "bind int64"
 
-bindNullError :: Statement -> Int -> IO Error
-bindNullError (Statement statement) parameterIndex = do
-  error <- c_sqlite3_bind_null statement (toParamIndex parameterIndex)
-  return $ decodeError error
 bindNull :: Statement -> Int -> IO ()
-bindNull statement parameterIndex = do
-  error <- bindNullError statement parameterIndex
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind null" error
+bindNull statement parameterIndex =
+    Direct.bindNull statement (toParamIndex parameterIndex)
+        >>= checkError Nothing "bind null"
 
-bindTextError :: Statement -> Int -> T.Text -> IO Error
-bindTextError (Statement statement) parameterIndex text = do
-  byteString <- return $ T.encodeUtf8 text
-  size <- return $ BS.length byteString
-  BS.useAsCString byteString
-                  (\dataC -> do
-                     error <- c_sqlite3_bind_text statement
-                                                  (toParamIndex parameterIndex)
-                                                  dataC
-                                                  (toCNumBytes size)
-                                                  c_SQLITE_TRANSIENT
-                     return $ decodeError error)
 bindText :: Statement -> Int -> T.Text -> IO ()
-bindText statement parameterIndex text = do
-  error <- bindTextError statement parameterIndex text
-  case error of
-    ErrorOK -> return ()
-    _ -> sqlError Nothing "bind text" error
+bindText statement parameterIndex text =
+    Direct.bindText statement
+                    (toParamIndex parameterIndex)
+                    (Utf8 $ T.encodeUtf8 text)
+        >>= checkError Nothing "bind text"
 
 bind :: Statement -> [SQLData] -> IO ()
 bind statement sqlData = do
@@ -288,35 +184,29 @@ bind statement sqlData = do
        $ zip [1..] sqlData
 
 columnType :: Statement -> Int -> IO ColumnType
-columnType (Statement statement) columnIndex =
-  decodeColumnType <$> c_sqlite3_column_type statement (toColumnIndex columnIndex)
+columnType statement columnIndex =
+    Direct.columnType statement (toColumnIndex columnIndex)
 
 columnBlob :: Statement -> Int -> IO BS.ByteString
-columnBlob (Statement statement) columnIndex = do
-  size <- c_sqlite3_column_bytes statement (toColumnIndex columnIndex)
-  BSI.create (fromCNumBytes size) $ \resultPtr -> do
-    dataPtr <- c_sqlite3_column_blob statement (toColumnIndex columnIndex)
-    if dataPtr /= nullPtr
-        then BSI.memcpy resultPtr dataPtr (fromCNumBytes size)
-        else return ()
+columnBlob statement columnIndex =
+    Direct.columnBlob statement (toColumnIndex columnIndex)
 
 columnInt64 :: Statement -> Int -> IO Int64
-columnInt64 (Statement statement) columnIndex = do
-  c_sqlite3_column_int64 statement (toColumnIndex columnIndex)
+columnInt64 statement columnIndex =
+    Direct.columnInt64 statement (toColumnIndex columnIndex)
 
 columnDouble :: Statement -> Int -> IO Double
-columnDouble (Statement statement) columnIndex = do
-  c_sqlite3_column_double statement (toColumnIndex columnIndex)
+columnDouble statement columnIndex =
+    Direct.columnDouble statement (toColumnIndex columnIndex)
 
 columnText :: Statement -> Int -> IO T.Text
-columnText (Statement statement) columnIndex = do
-  text <- c_sqlite3_column_text statement (toColumnIndex columnIndex)
-  byteString <- BS.packCString text
-  return $ T.decodeUtf8 byteString
+columnText statement columnIndex = do
+    Utf8 bs <- Direct.columnText statement (toColumnIndex columnIndex)
+    return $! T.decodeUtf8 bs
 
 columnCount :: Statement -> IO Int
-columnCount (Statement statement) =
-  fromColumnCount <$> c_sqlite3_column_count statement
+columnCount statement =
+    fromColumnCount <$> Direct.columnCount statement
 
 column :: Statement -> Int -> IO SQLData
 column statement columnIndex = do
